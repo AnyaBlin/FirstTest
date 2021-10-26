9e6c7cd64df405e43299b5b028f1cf80d38ec0bd
Anna <ariisutyan@gmail.com>
2021-10-26 13:15:03 +0300

    added test

---
+++ src/test/java/guru/qa/TestingForm.java
@@ -0,0 +1,34 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestingForm {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void FillFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Ann");
+        $("#lastName").setValue("Blini");
+        $("#userEmail").setValue("blinchiki@smetana.ru");
+        //$("#custom-control-input").;
+        $(By.name("gender")).setValue("Male");
+        $("#userNumber").setValue("88005553535");
+
+
+        $("#submit").click();
+
+
+    }
+}
